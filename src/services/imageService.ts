93701c76649606b821802a5f633ac960afb37490
gamer295 <hazar.qader@gmail.com>
2026-05-16 13:48:26 +0300

    feat(profile): Improve image upload for native platforms
    Refactors image picking logic to ensure native-specific handling. This includes using Capacitor's native image picker and directly setting the `source` for the camera plugin to 'Photos' when on a native platform. Additionally, the `noClick` prop for `useDropzone` is now conditionally applied only for native platforms, allowing web users to click the drop zone to upload.

--- src/services/imageService.ts
+++ src/services/imageService.ts
@@ -26,7 +26,7 @@ export const pickImage = async (isMultiple: boolean = false): Promise<string[]> 
           quality: 50,
           allowEditing: false,
           resultType: CameraResultType.DataUrl,
-          source: CameraSource.Prompt
+          source: CameraSource.Photos
         });
         return [image.dataUrl || ''];
       }
